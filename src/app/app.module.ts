8407d0d2b639d849ca47216455fa7d2d78852948
trvani <47416856+trvani@users.noreply.github.com>
2022-09-12 23:37:10 +0300

    Navbar front end
    Offcanvas is created in navbar for now.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AddPostComponent } from './components/add-post/add-post.component';
+import { EditPostComponent } from './components/edit-post/edit-post.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    SidebarComponent,
+    AddPostComponent,
+    EditPostComponent,
+    PostDetailsComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,23 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    firebaseConfig: {
+    apiKey: "AIzaSyAW_EYcxJx3G3icsT-4vjqQbekj68KaPuc",
+    authDomain: "blogapp-6dfef.firebaseapp.com",
+    projectId: "blogapp-6dfef",
+    storageBucket: "blogapp-6dfef.appspot.com",
+    messagingSenderId: "1060519877105",
+    appId: "1:1060519877105:web:d5939683caaee2cb414c50",
+    measurementId: "G-K1L4P2PMNS"
+  }
+
+  
 };
+
+ 
 
 /*
  * For easier debugging in development mode, you can import the following file
