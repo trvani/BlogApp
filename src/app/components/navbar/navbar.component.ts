4750310deddeb60a46c6be67f42c0a153c81a1d6
trvani <47416856+trvani@users.noreply.github.com>
2022-10-30 21:19:35 +0300

    Styling
    Navbar and dashboard header color are changed.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  isMenuCollapsed: boolean = false;
+  isMenuCollapsed: boolean = true;
 
   constructor() { }
 
